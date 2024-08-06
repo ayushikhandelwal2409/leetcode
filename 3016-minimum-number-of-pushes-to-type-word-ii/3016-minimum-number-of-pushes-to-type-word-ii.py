06fffc61b213aa580e73d23aad2e579c3de157e8
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2024-08-06 09:12:06 +0530

    Time: 207 ms (97.50%), Space: 13.2 MB (42.50%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -4,19 +4,14 @@ class Solution(object):
         :type word: str
         :rtype: int
         """
-    # Count the frequency of each character
         freq = {}
         for char in word:
             if char in freq:
                 freq[char] += 1
             else:
                 freq[char] = 1
-
-        # Sort the frequencies in descending order
         sorted_freq = sorted(freq.values())
         sorted_freq.reverse()
-
-        # Calculate the minimum number of pushes
         pushes = 0
         for i in range(len(sorted_freq)):
             pushes += (i // 8 + 1) * sorted_freq[i]

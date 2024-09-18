078ef3842c279ff05a859df89ba5b3b7b9931ea8
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2024-09-18 08:13:51 +0530

    Time: 1290 ms (18.92%), Space: 0B (100.00%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,10 @@
+SELECT 
+  R.contest_id, 
+  ROUND((COUNT(R.user_id) / (SELECT COUNT(*) FROM Users)) * 100, 2) AS percentage
+FROM 
+  Register R
+GROUP BY 
+  R.contest_id
+ORDER BY 
+  percentage DESC, 
+  contest_id ASC;

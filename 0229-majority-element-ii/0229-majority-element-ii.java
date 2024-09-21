0e910201380c11a0ea83b79266b0f080ba6c8c71
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2024-09-21 09:30:54 +0530

    Time: 2 ms (99.53%), Space: 48 MB (50.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,26 +2,43 @@ import java.util.*;
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
+        int k = nums.length / 3;
+        int val1 = nums[0], val2 = nums[0];
+        int in1 = 1, in2 = 0;
+        ArrayList<Integer> list = new ArrayList<>();
 
-        // Count the occurrences of each element
-        for (int num : nums) {
-            if (countMap.containsKey(num)) {
-                countMap.put(num, countMap.get(num) + 1);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == val1) {
+                in1++;
+            } else if (nums[i] == val2) {
+                in2++;
+            } else if (in1 == 0) {
+                val1 = nums[i];
+                in1 = 1;
+            } else if (in2 == 0) {
+                val2 = nums[i];
+                in2 = 1;
             } else {
-                countMap.put(num, 1);
+                in1--;
+                in2--;
             }
         }
 
-        // Find the elements that occur more than n/3 times
-        int threshold = nums.length / 3;
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            if (entry.getValue() > threshold) {
-                result.add(entry.getKey());
+        int count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == val1) {
+                count1++;
+            } else if (num == val2) {
+                count2++;
             }
         }
 
-        return result;
+        if (count1 > k) {
+            list.add(val1);
+        }
+        if (count2 > k) {
+            list.add(val2);
+        }
+        return list;
     }
 }

1e814a98f37754687b26ff9b21f5c4931172c491
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2024-11-04 15:51:31 +0530

    Time: 2110 ms (5.08%), Space: 51.4 MB (14.38%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,19 +1,20 @@
 class Solution {
     public String compressedString(String word) {
-        StringBuilder s = new StringBuilder();
+        String s="";
         char c=word.charAt(0);
         int ct=1;
         for(int i=1;i<word.length();i++){
             if(word.charAt(i)==c &&ct<9){
                 ct++;
             }else{
-                s.append(ct);
-                s.append(c);
+                s+=ct;
+                s+=c;
                 c=word.charAt(i);
                 ct=1;
             }
         }
-        s.append(ct).append(c);
-        return s.toString();
+        s+=ct;
+        s+=c;
+        return s;
     }
 }

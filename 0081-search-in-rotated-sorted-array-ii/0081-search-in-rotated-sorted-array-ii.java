cf65ac4271c00a594cc3d20947037a4327ca5a13
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2024-11-05 21:45:39 +0530

    Time: 7 ms (5.87%), Space: 44.5 MB (6.88%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int in=-1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                in=i;
+                break;
+            }
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=in+1;i<nums.length;i++){
+            list.add(nums[i]);
+        }
+        for(int i=0;i<=in;i++){
+            list.add(nums[i]);
+        }
+        int s=0;
+        int e=list.size() - 1;
+        System.out.println(list);
+        boolean k=false;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(list.get(mid)>target) e=mid-1;
+            else if(list.get(mid)==target){
+                k=true;
+                break;
+            }
+            else s=mid+1;
+        }
+        return k;
+    }
+}

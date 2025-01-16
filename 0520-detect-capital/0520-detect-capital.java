6f5a98f05bf89f2cb5904590790eb614bd247090
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-01-16 22:23:24 +0530

    Time: 0 ms (100%), Space: 41.6 MB (95.21%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -13,7 +13,7 @@ class Solution {
             }
         }
         if(l==s || l==c) return true;
-        if( word.charAt(0)>='A' && word.charAt(0)<='Z' && c==1 && s==l-1) return true;
+        if( word.charAt(0)>='A' && word.charAt(0)<='Z' && c==1) return true;
         return false;
     }
 }

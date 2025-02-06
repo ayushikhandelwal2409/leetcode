3350b67de7ef03f3d0778370a73be82d08638f94
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-02-06 12:45:37 +0530

    Time: 3 ms (21.33%), Space: 43.6 MB (61.19%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ll=new  ArrayList<>();
-        List<Integer> temp=new  ArrayList<>();
+        //List<List<Integer>> ll=new  ArrayList<>();
+        HashSet<List<Integer>> set = new HashSet<>();
+        //List<Integer> temp=new  ArrayList<>();
         Arrays.sort(nums);
-        print(nums,ll,temp,0);
-        return ll;
+        print(nums,set,new ArrayList<>(),0);
+        //return ll;
+        return new ArrayList<>(set);
 
     }
-    public static void print(int[] nums,List<List<Integer>>ll,List<Integer>temp,int start){
-        ll.add(new ArrayList<>(temp));
+    public static void print(int[] nums,HashSet<List<Integer>> set,List<Integer>temp,int start){
+        set.add(new ArrayList<>(temp));
 
         for(int i=start;i<nums.length;i++){
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue; 
-            }
+            // if (i > start && nums[i] == nums[i - 1]) {
+            //     continue; 
+            // }
             temp.add(nums[i]);
-            print(nums,ll,temp,i+1);
+            print(nums,set,temp,i+1);
 
             temp.remove(temp.size()-1);//backtrack
         }

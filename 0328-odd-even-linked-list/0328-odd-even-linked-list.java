37a04bc75b665c6d9a572e8479bedb0ebea2fbad
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-03-25 15:38:55 +0530

    Time: 0 ms (100%), Space: 44.6 MB (63.35%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,20 +10,33 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null){
-            return null;
+        if (head == null || head.next == null) {
+            return head; // If the list is empty or has only one node
         }
-        if(head.next==null) return head;
-        ListNode odd=head;
-        ListNode even=head.next;
-        ListNode evenHead=even;
-        while(even!=null && even.next!=null){
-            odd.next=odd.next.next;
-            odd=odd.next;
-            even.next=odd.next;
-            even=even.next;
+
+        ListNode oddHead = new ListNode(0); // Dummy head for odd nodes
+        ListNode evenHead = new ListNode(0); // Dummy head for even nodes
+        ListNode odd = oddHead;
+        ListNode even = evenHead;
+        ListNode current = head;
+        int index = 1; // Start with index 1 for the first node
+
+        while (current != null) {
+            if (index % 2 != 0) { // Odd index
+                odd.next = current;
+                odd = odd.next;
+            } else { // Even index
+                even.next = current;
+                even = even.next;
+            }
+            current = current.next;
+            index++;
         }
-        odd.next=evenHead;
-        return head;
+
+        // Connect the end of odd list to the head of even list
+        odd.next = evenHead.next;
+        even.next = null; // End the even list
+
+        return oddHead.next; // Return the head of the rearranged list
     }
 }

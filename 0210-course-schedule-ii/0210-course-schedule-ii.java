055d4e0261cc26e62d31e883bf476ac139ee3431
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-06-09 22:17:15 +0530

    Time: 6 ms (51.43%), Space: 46.3 MB (29.57%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int V = numCourses;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Fix: edge[1] -> edge[0], since course a needs b first
+        for (int[] edge : prerequisites) {
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        int[] indegree = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int j : adj.get(i)) {
+                indegree[j]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int[] topo=new int[V];
+        int i=0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo[i++]=node;
+            for (int neighbor : adj.get(node)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.offer(neighbor);
+                }
+            }
+        }
+
+        if(i == V) return topo;
+        int[] arr={};
+        return arr;
+    }
+}

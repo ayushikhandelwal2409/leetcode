d6f3d6164ce9a240bf8a2e7cfea6cb47419612eb
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-06-13 13:39:11 +0530

    Time: 395 ms (5.08%), Space: 60.9 MB (39.83%) - LeetHub

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/3225-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/3225-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,11 +1,8 @@
 class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        int n=nums.length;
         HashMap<Integer,Integer> map=new HashMap<>();
-        int max=0;
-        int start=0;
-        int i=0;
-        while(i<n){
+        int max=0,start=0,i=0;
+        while(i<nums.length){
             map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
             while(map.get(nums[i])>k){
                 map.put(nums[start],map.get(nums[start])-1);

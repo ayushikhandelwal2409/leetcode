229b0c7867c5ae9a7aeed5dd07525352f7821d67
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-06-27 18:23:01 +0530

    Time: 0 ms (100%), Space: 43 MB (96.74%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,30 +1,36 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int in=-1;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
-                in=i;
-                break;
-            }
-        }
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=in+1;i<nums.length;i++){
-            list.add(nums[i]);
-        }
-        for(int i=0;i<=in;i++){
-            list.add(nums[i]);
-        }
+        int n=nums.length;
         int s=0;
-        int e=list.size() - 1;
-       // System.out.println(list);
+        int e=n-1;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(list.get(mid)>target) e=mid-1;
-            else if(list.get(mid)==target){
-                return true;
+            if(nums[mid]==target)return true;;
+            if(nums[s]==nums[mid] && nums[e]==nums[mid]){
+                s=s+1;
+                e=e-1;
+                continue;
             }
-            else s=mid+1;
+            //check for sorted side-- right or left half
+            if(nums[s]<=nums[mid]){  //sorted left half
+                if(nums[s]<=target && target<=nums[mid]){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(nums[e]>=target &&target >=nums[mid]){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
         }
         return false;
     }
 }
+
+

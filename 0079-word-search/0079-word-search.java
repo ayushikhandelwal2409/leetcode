989219af66ebe27b2b4c89c46a41cfa6d91d89ca
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-07-10 14:42:32 +0530

    Time: 269 ms (12.11%), Space: 45.4 MB (5.28%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,32 +2,31 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int n=board.length;
         int m=board[0].length;
-        int[][] vis=new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j] == word.charAt(0) && dfs(board,word,i,j,0,vis) )return true;  // 0==>index of string {word}
+                if(board[i][j] == word.charAt(0) && dfs(board,word,i,j,0) )return true;  // 0==>index of string {word}
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board,String word,int row,int col,int idx,int[][] vis){
+    public boolean dfs(char[][] board,String word,int row,int col,int idx){
         int n=board.length;
         int m=board[0].length;
         if(idx==word.length()) return true;
-        if(row<0 || row>=n ||col<0 || col>=m|| vis[row][col]==1 || board[row][col]!=word.charAt(idx)){
+        if(row<0 || row>=n ||col<0 || col>=m || board[row][col]!=word.charAt(idx)){
             return false;
         }
-        vis[row][col]=1;
+        char value=board[row][col];
+        board[row][col]='-';
         int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         for (int[] dir : directions) {
             int newRow = row + dir[0];
             int newCol = col + dir[1];
-            if (dfs(board, word,newRow, newCol, idx + 1,vis)) {
-                // vis[row][col] = 0; // backtrack
+            if (dfs(board, word,newRow, newCol, idx + 1)) {
                 return true;
             }
         }
-        vis[row][col]=0;
+        board[row][col]=value;
         return false;
     }
 }

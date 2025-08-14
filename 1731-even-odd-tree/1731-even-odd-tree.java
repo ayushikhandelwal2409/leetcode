e36cf19d6bfc41ceea53746aaea2200d86c7b304
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-08-14 19:49:30 +0530

    Time: 9 ms (98.14%), Space: 65.9 MB (28.26%) - LeetHub

---
+++ 1731-even-odd-tree/1731-even-odd-tree.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        //ArrayList<ArrayList<TreeNode>> list=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        boolean evenlevel=true;
+        while(!q.isEmpty()){
+            int size=q.size();
+            //ArrayList<TreeNode> k=new ArrayList<>();
+            int prevval=evenlevel?Integer.MIN_VALUE:Integer.MAX_VALUE;
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                //k.add(node);
+                 if (evenlevel) {
+                    // Even level → values must be odd and strictly increasing
+                    if (node.val % 2 == 0 || node.val <= prevval) return false;
+                } else {
+                    // Odd level → values must be even and strictly decreasing
+                    if (node.val % 2 != 0 || node.val >= prevval) return false;
+                }
+                prevval=node.val;
+                if(node.left!=null){
+                    q.offer(node.left);
+                }
+                if(node.right!=null){
+                    q.offer(node.right);
+                }
+            }
+            //list.add(new ArrayList<>(k));
+            evenlevel=!evenlevel;
+        }
+        return true;
+    }
+    // public static boolean check(ArrayList<ArrayList<TreeNode>> list){
+    //     int n=list.size();
+    //     for(int i=0;i<n;i+=2){
+    //         for(int j=0;j<list.get(i).size();j++){
+    //             if(list.get(i).get(j).val%2==0) return false;
+    //             if(j!=0){
+    //                 if(list.get(i).get(j-1).val>list.get(i).get(j).val) return false;
+    //             }
+    //         }
+    //     }
+    //     for(int i=1;i<n;i+=2){
+    //         for(int j=0;j<list.get(i).size();j++){
+    //             if(list.get(i).get(j).val%2!=0) return false;
+    //             if(j!=0){
+    //                 if(list.get(i).get(j-1).val<list.get(i).get(j).val) return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+}

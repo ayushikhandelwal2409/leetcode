cab676a3ba092cc73ecf668fa9ed7fa73088cd3f
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-09-05 17:07:54 +0530

    Time: 3 ms (94.54%), Space: 44.9 MB (88.27%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
+        }
+        return min_operation(word1,word2,0,0,dp);
+    }
+    public static int min_operation(String s,String t,int i,int j,int[][] dp){
+        if (i == s.length()) return t.length() - j; // need to insert remaining chars
+        if (j == t.length()) return s.length() - i;// need to delete remaining chars
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int ans=0;
+        if(s.charAt(i)==t.charAt(j)){
+            ans=min_operation(s, t, i+1,j+1,dp);
+        }
+        else{
+            int delete = min_operation(s, t, i +  1, j,dp);
+            int replace = min_operation(s, t, i +  1, j + 1,dp);
+            int insert = min_operation(s, t, i , j + 1,dp);
+            ans = Math.min(delete, Math.min(replace, insert)) + 1;
+        }
+        return dp[i][j]=ans;
+    }
+}

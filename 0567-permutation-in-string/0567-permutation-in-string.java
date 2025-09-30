f14497f50365eebe9f7b50950b0b77563247f75d
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-09-30 11:52:59 +0530

    Time: 6 ms (85.03%), Space: 42.8 MB (69.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -14,17 +14,21 @@ class Solution {
             s1Count[c - 'a']++;
         }
         
-        for (int i = 0; i < len1; i++) {
-            s2Count[s2.charAt(i) - 'a']++;
-        }
+        // for (int i = 0; i < len1; i++) {
+        //     s2Count[s2.charAt(i) - 'a']++;
+        // }
         
-        if (Arrays.equals(s1Count, s2Count)) {
-            return true;
-        }
-        
-        for (int i = len1; i < len2; i++) {
+        // if (Arrays.equals(s1Count, s2Count)) {
+        //     return true;
+        // }
+        int start=0;
+        for (int i = 0; i < len2; i++) {
             s2Count[s2.charAt(i) - 'a']++;
-            s2Count[s2.charAt(i - len1) - 'a']--;
+            //s2Count[s2.charAt(i - len1) - 'a']--;
+            if(i-start+1>len1){
+                s2Count[s2.charAt(start) - 'a']--;
+                start++;
+            }
             
             if (Arrays.equals(s1Count, s2Count)) {
                 return true;

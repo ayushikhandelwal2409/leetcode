6990c568f0cc16ed8fdf0f2da828c608d945d8e8
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-10-01 15:00:03 +0530

    Time: 1 ms (99.64%), Space: 45.2 MB (61.69%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,37 @@
+// class Solution {
+//     public int jump(int[] nums) {
+//         int jumps=0;
+//         int l=0;
+//         int r=0;
+//         while(r<nums.length-1){
+//             int farthest=0;
+//             for(int i=l;i<=r;i++){
+//                 farthest=Math.max(i+nums[i],farthest);
+//             }
+//             l=r+1;
+//             r=farthest;
+//             jumps+=1;
+//         }
+//         return jumps;
+//     }
+// }
+
+// //method 2
+
+
 class Solution {
     public int jump(int[] nums) {
         int jumps=0;
-        int l=0;
-        int r=0;
-        while(r<nums.length-1){
-            int farthest=0;
-            for(int i=l;i<=r;i++){
-                farthest=Math.max(i+nums[i],farthest);
+        int i=0;
+        int max_jump=0;
+        int endJump=0;
+        while(i<nums.length-1){
+            max_jump=Math.max(i+nums[i],max_jump);
+            if(i==endJump){
+                jumps++;
+                endJump=max_jump;
             }
-            l=r+1;
-            r=farthest;
-            jumps+=1;
+            i++;
         }
         return jumps;
     }

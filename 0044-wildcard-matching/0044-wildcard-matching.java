13c35695fbeb0c1042d9022d6152275130f7a22f
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-10-07 06:27:53 +0530

    Time: 1 ms (100%), Space: 43.8 MB (97.18%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int i=0;//s
         int j=0;//p
-        int starIdx = -1;  // last position of '*' in p
-        int iIdx = -1; // position in s when '*' was found
+        int lastStar = -1;  // last position of '*' in p
+        int lastMatch = 0; // position in s when '*' was found
          while (i < s.length()) {
             // Case 1: if current chars match or pattern has '?'
             if (j < p.length() && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?')) {
@@ -12,15 +12,15 @@ class Solution {
             }
             // Case 2: pattern has '*'
             else if (j < p.length() && p.charAt(j) == '*') {
-                starIdx = j;   // mark position of '*'
-                iIdx = i;      // mark position in s
+                lastStar = j;   // mark position of '*'
+                lastMatch = i;      // mark position in s
                 j++;           // move pattern pointer ahead
             }
             // Case 3: mismatch but we had a previous '*'
-            else if (starIdx != -1) {
-                j = starIdx + 1;  // backtrack pattern to after '*'
-                iIdx++;           // let '*' absorb one more char
-                i = iIdx;         // move i forward
+            else if (lastStar != -1) {
+                j = lastStar + 1;  // backtrack pattern to after '*'
+                lastMatch++;           // let '*' absorb one more char
+                i = lastMatch;         // move i forward
             }
             // Case 4: no match and no '*' to backtrack to
             else {

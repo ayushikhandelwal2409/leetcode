8b2e0a39b3928be52cdd4501629baea930df7791
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-10-08 07:57:13 +0530

    Time: 0 ms (100%), Space: 41.1 MB (63.73%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int[] max_diff = new int[n];
 
-        for (int i = n - 2; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             max_diff[i] = nums[i];
             for (int j = i + 1; j < n; j++) {
                 max_diff[j] = Math.max(nums[i] - max_diff[j], nums[j] - max_diff[j - 1]);

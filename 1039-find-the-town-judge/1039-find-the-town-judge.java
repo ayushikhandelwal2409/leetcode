53bd8a0cd6121f71deaedb279c519be511227b0f
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-10-11 14:07:16 +0530

    Time: 3 ms (66.43%), Space: 52.8 MB (90.89%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,16 +1,40 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(n==1) return 1;
-        int[] arr=new int[n+1];
-        for(int[] l:trust){
-            arr[l[0]]--;
-            arr[l[1]]++;
+        int[][] trustInfo = new int[n + 1][2]; 
+        for (int[] t : trust) {
+            trustInfo[t[0]][0]++; // person t[0] trusts someone
+            trustInfo[t[1]][1]++; // person t[1] is trusted by someone
         }
+
         for (int i = 1; i <= n; i++) {
-            if (arr[i] == n - 1) {
+            if (trustInfo[i][0] == 0 && trustInfo[i][1] == n - 1) {
                 return i; 
             }
         }
+
         return -1;
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     public int findJudge(int n, int[][] trust) {
+//         if(n==1) return 1;
+//         int[] arr=new int[n+1];
+//         for(int[] l:trust){
+//             arr[l[0]]--;
+//             arr[l[1]]++;
+//         }
+//         for (int i = 1; i <= n; i++) {
+//             if (arr[i] == n - 1) {
+//                 return i; 
+//             }
+//         }
+//         return -1;
+//     }
+// }

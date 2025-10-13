a0e28fec061eceac2467ebc7c6bc8459dfdf6096
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-10-13 20:04:23 +0530

    Time: 11 ms (67.75%), Space: 45.5 MB (37.35%) - LeetHub

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String answerString(String word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        String largest="";
+        int n=word.length();
+        int my_string_len=n-numFriends+1;
+        for(int i=0;i<n;i++){
+            String sub = word.substring(i,Math.min(i+my_string_len, n));
+            if (sub.compareTo(largest) > 0) {
+                largest = sub;
+            }
+        }
+        return largest;
+    }
+}

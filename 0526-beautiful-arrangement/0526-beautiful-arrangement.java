49a618fa8c42ab9bcac78ee966379917bc25280f
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-10-14 05:37:31 +0530

    Time: 38 ms (69.13%), Space: 40.8 MB (31.95%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -1,25 +1,23 @@
 class Solution {
     int count=0;
     public int countArrangement(int n) {
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) nums[i] = i+1;
-        permute(nums, 0);
+        boolean[] visited = new boolean[n + 1];
+        backtrack(n, 1, visited);
         return count;
     }
-    public void permute(int[] nums, int idx) {
-        if (idx == nums.length ) {
-            count++;
-        }
-        for (int i = idx; i < nums.length; i++) {
-            swap(nums, i, idx);
-            if (nums[idx] % (idx + 1) == 0 || (idx + 1) % nums[idx] == 0) permute(nums,idx+ 1);
-            swap(nums, i, idx);
-        }
-    }
 
-    public void swap(int[] nums, int x, int y) {
-        int temp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = temp;
+    private void backtrack(int n, int pos, boolean[] visited) {
+        if (pos > n) {
+            count++;
+            return;
+        }
+
+        for (int num = 1; num <= n; num++) {
+            if (!visited[num] && (num % pos == 0 || pos % num == 0)) {
+                visited[num] = true;
+                backtrack(n, pos + 1, visited);
+                visited[num] = false;
+            }
+        }
     }
 }

9fb6696b18f0fb730971750b7aea341762cd4196
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2025-12-26 16:48:42 +0530

    Time: 1734 ms (27.54%), Space: 42.6 MB (31.16%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.java
+++ 3133-minimum-array-end/3133-minimum-array-end.java
@@ -1,7 +1,7 @@
 class Solution {
     public long minEnd(int n, int x) {
         long result= x;
-        while(n-1>0){
+        while(n-1>0){// we use n-1 b'coz from n len we have already one element as x
             result=x|(result+1);
             n--;
         }

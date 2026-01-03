00bed44a2515d91e1c39bfcfa2f430c7044ef472
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2026-01-03 11:49:47 +0530

    Time: 131 ms (22.06%), Space: 48.4 MB (52.1%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -10,6 +10,7 @@ class Solution {
         while(!q.isEmpty()){
             String[] str=q.poll();
             String word=str[0];
+            
             int steps=Integer.valueOf(str[1]);
             if(word.equals(endWord)) return steps;
             for(int i=0;i<word.length();i++){
@@ -19,6 +20,7 @@ class Solution {
                     String replacedWord=new  String(arr);
                     if(set.contains(replacedWord)){
                         set.remove(replacedWord);
+                        System.out.println(word);
                         int k=steps+1;
                         q.offer(new String[]{replacedWord,String.valueOf(k)});
 

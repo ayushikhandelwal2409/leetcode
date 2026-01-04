7b950700344c590906cd3e2739742cb03fe1cee6
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2026-01-04 13:17:23 +0530

    Time: 103 ms (19.37%), Space: 46.6 MB (25.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,57 +1,19 @@
-// class Solution {
-//     public int lengthOfLIS(int[] nums) {
-//         return LIS(nums);
-//     }
-//     public static int LIS(int[] arr){
-//         int[] dp=new int[arr.length];
-//         Arrays.fill(dp,1);
-//         for (int i = 0; i < arr.length; i++) {
-//             for (int j = i; j >=0; j--) {
-//                 if(arr[i]>arr[j]) {
-//                     int val = dp[j];
-//                     dp[i]=Math.max(dp[i],val+1);
-//                 }
-//             }
-//         }
-//         return Arrays.stream(dp).max().getAsInt();
-//     }
-// }
-
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return LIS(nums);
+        int max=0;
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[j]+1,dp[i]);
+                }
+            }
+            max=Math.max(max,dp[i]);
+            System.out.println(max);
+        }
+        return max;        
     }
-    public static int LIS(int [] arr) {
-		int dp[]=new int[arr.length];
-		int len=1;
-		dp[0]=arr[0];
-		for(int i=1;i<dp.length;i++) {
-//			adding 
-			if(arr[i]>dp[len-1]) {
-				dp[len]=arr[i];
-				len++;
-			}
-			else {
-//				replacing
-				int idx=BinarySearch(dp,0,len-1,arr[i]);
-				dp[idx]=arr[i];
-			}
-		}
-		return len;
-	}
-
-	public static int BinarySearch(int [] dp,int si,int ei,int item) {
-		int idx=0;
-		while(si<=ei) {
-			int mid=(si+ei)/2;
-			if(dp[mid]>=item) {
-				idx=mid;
-				ei=mid-1;
-			}
-			else {
-				si=mid+1;
-			}
-		}
-		return idx;
-	}
+    
 }

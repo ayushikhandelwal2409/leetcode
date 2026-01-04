ee3c2b0d439f9210e9a8d8aa5952b259f71c3498
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2026-01-04 12:24:05 +0530

    Time: 17 ms (32.78%), Space: 46.5 MB (26.36%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -2,21 +2,53 @@ class Solution {
     public int countSubstrings(String s) {
         int n=s.length();
         int cnt=0;
+        boolean[][] dp=new boolean[n][n];
+        //1 substring
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++)
-            cnt+=palindromic(s,i,j);
+            dp[i][i]=true;
+            cnt++;
+        }
+        //2  length substring
+        for(int i=0;i<n-1;i++){
+            if(s.charAt(i)==s.charAt(i+1)){
+                dp[i][i+1]=true;
+                cnt++;
+            }
+        }
+        //for length >=3 substring
+        for(int len=2;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]){
+                    dp[i][j]=true;
+                    cnt++;
+                }
+            }
         }
         return cnt;
     }
-    public static int palindromic(String s,int st,int end){
-        while(st<end){
-            if(s.charAt(st)!=s.charAt(end)){
-                return 0;
-            }
-            st++;
-            end--;
-        }
-        return 1;
-    }
-
 }
+
+//bruteforce
+// class Solution {
+//     public int countSubstrings(String s) {
+//         int n=s.length();
+//         int cnt=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++)
+//             cnt+=palindromic(s,i,j);
+//         }
+//         return cnt;
+//     }
+//     public static int palindromic(String s,int st,int end){
+//         while(st<end){
+//             if(s.charAt(st)!=s.charAt(end)){
+//                 return 0;
+//             }
+//             st++;
+//             end--;
+//         }
+//         return 1;
+//     }
+
+// }

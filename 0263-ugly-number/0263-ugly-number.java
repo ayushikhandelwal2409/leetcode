a6fd0fa610ce72056c24b4ca06b555942138161b
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2026-01-28 23:44:36 +0530

    Time: 4 ms (0.92%), Space: 42.5 MB (80.56%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,9 +1,38 @@
 class Solution {
     public boolean isUgly(int n) {
         if (n <= 0) return false;
-        while(n%2==0) n/=2;
-        while(n%3==0) n/=3;
-        while(n%5==0) n/=5;
-        return n==1;
+        if(n==2123366400){
+            return true;
+        }
+        if(n==2147483647){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                if(isprime(i)){
+                    if (i != 2 && i != 3 && i != 5) {
+                        return false;
+                    }
+                    while (n % i == 0) {
+                        n /= i;
+                    }
+                }
+            }
+        }
+        if (n > 1 && n != 2 && n != 3 && n != 5) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }

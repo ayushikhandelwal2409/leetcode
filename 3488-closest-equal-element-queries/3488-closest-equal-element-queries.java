f334bfd42c002e61ad0a76d06b63047bd503120e
Ayushi Khandelwal <ayushikhandelwal2406@gmail.com>
2026-04-16 21:39:54 +0530

    Time: 173 ms (14.14%), Space: 182.2 MB (86.87%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
@@ -4,33 +4,37 @@ class Solution {
     public List<Integer> solveQueries(int[] nums, int[] queries) {
         int n = nums.length;
 
-        // Step 1: Map value → indices
         Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            if(map.containsKey(nums[i])){ 
+                map.get(nums[i]).add(i); 
+            } 
+            else{ 
+                map.put(nums[i], new ArrayList<>()); 
+                map.get(nums[i]).add(i); 
+            }
         }
 
         List<Integer> ans = new ArrayList<>();
 
-        // Step 2: Process queries
         for (int q : queries) {
             List<Integer> list = map.get(nums[q]);
 
             if (list.size() == 1) {
                 ans.add(-1);
-                continue;
             }
+            else{
+             int pos = Collections.binarySearch(list, q);
 
-            int pos = Collections.binarySearch(list, q);
+                // neighbors in circular manner
+                int left = (pos - 1 + list.size()) % list.size();
+                int right = (pos + 1) % list.size();
 
-            // neighbors in circular manner
-            int left = (pos - 1 + list.size()) % list.size();
-            int right = (pos + 1) % list.size();
+                int dist1 = getDist(q, list.get(left), n);
+                int dist2 = getDist(q, list.get(right), n);
 
-            int dist1 = getDist(q, list.get(left), n);
-            int dist2 = getDist(q, list.get(right), n);
-
-            ans.add(Math.min(dist1, dist2));
+                ans.add(Math.min(dist1, dist2));
+            }
         }
 
         return ans;
